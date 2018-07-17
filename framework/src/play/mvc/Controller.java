d734b03984ec4e732dd04772fe8cce7493e0e98a
Gregory B <dev@someilfe.eu>
2018-07-17 13:56:59 +0200

    fix(tests): issue #1200, added testing compatibility for jdk9+
    refactor(tests): issue #1200, adds Controller.test class and unit test for modification on this issue
    
    fix(tests): issue #1200, fixes missing imports
    
    fix(tests): issue #1200, fixes encapsulation
    
    fix(tests): issue #1200 sorted import errors
    
    style(tests): issue #1200 beautification

--- framework/src/play/mvc/Controller.java
+++ framework/src/play/mvc/Controller.java
@@ -1248,7 +1248,7 @@ public class Controller implements ControllerSupport, LocalVariablesSupport {
                 }
 
                 if (haveSeenFirstApplicationClass) {
-                    if (className.startsWith("sun.") || className.startsWith("play.")) {
+                    if (shouldBeCheckedForEnhancement(className)) {
                         // we're back into the play framework code...
                         return; // done checking
                     } else {
@@ -1264,6 +1264,17 @@ public class Controller implements ControllerSupport, LocalVariablesSupport {
             }
 
         }
+    }
+
+
+    /**
+     * Checks if the classname is from the jdk, sun or play package and therefore should not be checked for enhancement
+     * @param String className
+     * @return boolean
+     */
+    static boolean shouldBeCheckedForEnhancement(String className)
+    {
+        return className.startsWith("jdk.") || className.startsWith("sun.") || className.startsWith("play.");
     }
 
     protected static <T> void await(Future<T> future, F.Action<T> callback) {
