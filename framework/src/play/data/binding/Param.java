cfc8b9230d3f05036fa06647519f61a5a0aed831
Mridang Agarwalla <mridang@nosto.com>
2018-09-14 16:26:59 +0300

    Added parameter annotation for the action-method param binder

---
+++ framework/src/play/data/binding/Param.java
@@ -0,0 +1,14 @@
+package play.data.binding;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Target(ElementType.PARAMETER)
+public @interface Param {
+    String value();
+}
